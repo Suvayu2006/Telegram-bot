292fdb2b8d882b27c9f61e0f312b8feb999559f7
Suvayu2006 <suvayughosh2006@gmail.com>
2026-05-05 15:40:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -198,4 +198,5 @@ def index():
     return '🎵 Xylomutronix Bot is running!'
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    flask_app.run(host='0.0.0.0', port=port)
